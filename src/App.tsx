4a477ecfd39869c8284c0dad4a9ccacf47fe5830
Mohammad Aghajani <mhmmd.aghjni@gmail.com>
2024-09-01 19:03:26 +0200

    feat: add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from "./theme/ThemeProvider";
 import Main from "./screens/Main";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://snowtooth.moonhighway.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <ThemeProvider>
-      <Main />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider>
+        <Main />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
