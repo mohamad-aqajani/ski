8aebeba4642abaffbe792d97592e2f365102dbf5
Mohammad Aghajani <mhmmd.aghjni@gmail.com>
2024-09-01 19:05:07 +0200

    chore: improve flex columns in the main screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,32 +1,50 @@
 import { FC } from "react";
-import { Button, Flex, Space } from "antd";
-import { SelectTrailLevel, Background, TrailsList, InputNumber } from "../components";
+import { Button, Flex, Modal, Space } from "antd";
+import {
+  SelectTrailLevel,
+  Background,
+  TrailsList,
+  InputNumber,
+} from "../components";
+import useMainScreen from "./useMainScreen";
+import styled from "styled-components";
 
 const Main: FC<unknown> = () => {
+  const { trails, groupSize, setGroupSize, setLevel, getTrails, loading } =
+    useMainScreen();
   return (
     <Background>
       <Space direction="vertical" align="center" size={"large"}>
-        <Flex
+        <StyledFlex
           vertical
           gap={12}
           align="center"
-          style={{ width: "450px", maxWidth: "95%" }}
         >
+          <h2>Find Your Perfect Trail!</h2>
           {/* Group size */}
-          <InputNumber />
+          <InputNumber
+            onChange={(e) => setGroupSize(Number(e))}
+            defaultValue={groupSize}
+          />
 
           {/* Trail difficulty level */}
-          <SelectTrailLevel onSelect={() => {}} />
+          <SelectTrailLevel onSelect={setLevel} />
 
           {/* Submit button */}
-          <Button type="primary" block>
-            Search Trails
+          <Button type="primary" block onClick={() => getTrails()} loading={loading}>
+            Load Trails
           </Button>
-        </Flex>
+        </StyledFlex>
 
+        <TrailsList trails={trails} />
       </Space>
     </Background>
   );
 };
 
 export default Main;
+
+const StyledFlex = styled(Flex)`
+  width: 450px;
+  max-width: 95vw;
+`;
